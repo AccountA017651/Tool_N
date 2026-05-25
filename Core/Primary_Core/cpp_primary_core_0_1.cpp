f90bd27a4c3d8b874c535009ec5a0c7d042164d7
AccountA017651 <atom.atomizer@atomicmail.io>
2026-05-25 12:38:37 +0800

    Add a loop so the program does not exit immediately.

--- Core/Primary_Core/cpp_primary_core_0_1.cpp
+++ Core/Primary_Core/cpp_primary_core_0_1.cpp
@@ -19,57 +19,60 @@ int main(int argc, char* argv[]) {
     std::string os_name = OSOU::Obtain_OS();
     std::string cpu_architecture_name = CPUARCHOU::Obtain_CPU_Architecture();
     bool command_used = false;
+    bool main_running = true;
 
-    if (argc < 2) {
-        TCU::Terminal_Error("No command provided, please provide a command.\n");
-        return 1;
-    }
-
-    // Flag handler
-    std::unordered_map<std::string, std::function<void()>> flag_map = {
-        // QoL flags
-        {"--version", [&](){ std::print("Tool_N version: {}.{}.{}.{}\n", CVU::VERSION_MAJOR, CVU::VERSION_MINOR, CVU::VERSION_FIX, CVU::VERSION_STAGE); }},
-        {"-v",        [&](){ std::print("Tool_N version: {}.{}.{}.{}\n", CVU::VERSION_MAJOR, CVU::VERSION_MINOR, CVU::VERSION_FIX, CVU::VERSION_STAGE); }},
-        {"--help",    [&](){ LN_PRINT::Help_Command_Output(); }},
-        {"-h",        [&](){ LN_PRINT::Help_Command_Output(); }}
-    };
-
-    // Command handler
-    std::unordered_map<std::string, std::function<void()>> command_map = {
-        // Misc commands
-        {"Misc-Predictable-Terminate-0",      [&](){ CHR::DFND_BHVR::Explicit_Abort(); } },
-        {"Misc-Unpredictable-Terminate-0",    [&](){ CHR::UNDN_BHVR::Divide_Zero(); } },
-        {"Misc-Unpredictable-Terminate-1",    [&](){ CHR::UNDN_BHVR::Null_Pointer_Dereference(42); } },
-        {"Misc-Unpredictable-Terminate-2",    [&](){ CHR::UNDN_BHVR::Stack_Overflow(); } },
-        {"Misc-Unpredictable-Terminate-3",    [&](){ CHR::UNDN_BHVR::Volatile_Assembly_Illegal_Instruction(); } },
-        // Info commands
-        {"Info-CPU-Architecture",             [&](){ std::print("CPU Architecture: {}", cpu_architecture_name); }},
-        {"Info-Operating-System",             [&](){ std::print("OS name: {}\n", os_name); }},
-        {"Computer-Vision-Open-Webcam",       [&](){ CVP::WOU::Open_Webcam(); }},
-        {"Computer-Vision-Face-Recognition",  [&](){ CVP::BFDU::Face_Recognition_Webcam(); }}
-    };
-
-
-    for (int index = 1; index < argc; index++) {
-        std::string arg = argv[index];
-
-        auto flag_it = flag_map.find(arg);
-        if (flag_it != flag_map.end()) {
-            flag_it->second();
-            continue;
+    while (main_running) {
+        if (argc < 2) {
+            TCU::Terminal_Error("No command provided, please provide a command.\n");
+            return 1;
         }
 
-        auto cmd_it = command_map.find(arg);
-        if (cmd_it != command_map.end()) {
-            cmd_it->second();
-            command_used = true;
-            continue;
+        // Flag handler
+        std::unordered_map<std::string, std::function<void()>> flag_map = {
+            // QoL flags
+            {"--version", [&](){ std::print("Tool_N version: {}.{}.{}.{}\n", CVU::VERSION_MAJOR, CVU::VERSION_MINOR, CVU::VERSION_FIX, CVU::VERSION_STAGE); }},
+            {"-v",        [&](){ std::print("Tool_N version: {}.{}.{}.{}\n", CVU::VERSION_MAJOR, CVU::VERSION_MINOR, CVU::VERSION_FIX, CVU::VERSION_STAGE); }},
+            {"--help",    [&](){ LN_PRINT::Help_Command_Output(); }},
+            {"-h",        [&](){ LN_PRINT::Help_Command_Output(); }}
+        };
+
+        // Command handler
+        std::unordered_map<std::string, std::function<void()>> command_map = {
+            // Misc commands
+            {"Misc-Predictable-Terminate-0",      [&](){ CHR::DFND_BHVR::Explicit_Abort(); } },
+            {"Misc-Unpredictable-Terminate-0",    [&](){ CHR::UNDN_BHVR::Divide_Zero(); } },
+            {"Misc-Unpredictable-Terminate-1",    [&](){ CHR::UNDN_BHVR::Null_Pointer_Dereference(42); } },
+            {"Misc-Unpredictable-Terminate-2",    [&](){ CHR::UNDN_BHVR::Stack_Overflow(); } },
+            {"Misc-Unpredictable-Terminate-3",    [&](){ CHR::UNDN_BHVR::Volatile_Assembly_Illegal_Instruction(); } },
+            // Info commands
+            {"Info-CPU-Architecture",             [&](){ std::print("CPU Architecture: {}", cpu_architecture_name); }},
+            {"Info-Operating-System",             [&](){ std::print("OS name: {}\n", os_name); }},
+            {"Computer-Vision-Open-Webcam",       [&](){ CVP::WOU::Open_Webcam(); }},
+            {"Computer-Vision-Face-Recognition",  [&](){ CVP::BFDU::Face_Recognition_Webcam(); }}
+        };
+
+
+        for (int index = 1; index < argc; index++) {
+            std::string arg = argv[index];
+
+            auto flag_it = flag_map.find(arg);
+            if (flag_it != flag_map.end()) {
+                flag_it->second();
+                continue;
+            }
+
+            auto cmd_it = command_map.find(arg);
+            if (cmd_it != command_map.end()) {
+                cmd_it->second();
+                command_used = true;
+                continue;
+            }
+
+
+            TCU::Terminal_Error("Unknown argument: " + arg + "\n");
+            return 1;
         }
 
-
-        TCU::Terminal_Error("Unknown argument: " + arg + "\n");
-        return 1;
+        return command_used ? 0 : 0;
     }
-
-    return command_used ? 0 : 0;
 }
